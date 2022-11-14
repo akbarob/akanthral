1a45fd72dcf92f501ffa536c44535c571cc5e512
Akbar <akbarbadmus07@gmail.com>
2022-11-14 13:12:53 +0100

    init

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="flex flex-col items-center justify-center h-screen">
+      <h1 className="font-bold text-7xl text-yellow-700">Akanthral</h1>
+      <p className="mt-32 italic txt-xl">Coming Soon ...</p>
+    </div>
+  );
+}
+
+export default App;
---
+++ tailwind.config.cjs
@@ -0,0 +1,29 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        quicksand: ["Quicksand", "sans-serif"],
+      },
+      animation: {
+        "slide-in": "slide-in 0.5s ease-out",
+        "slide-fwd":
+          " slide-fwd 0.45s cubic-bezier(0.250, 0.460, 0.450, 0.940) both",
+      },
+      keyframes: {
+        "slide-in": {
+          "0%": {
+            "-webkit-transform": "translateX(-200px)",
+            transform: "translateX(-200px)",
+          },
+          "100%": {
+            "-webkit-transform": "translateX(0px)",
+            transform: "translateX(0px)",
+          },
+        },
+      },
+    },
+  },
+  plugins: [],
+};
